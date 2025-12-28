d3f9ee99a098630d05474c45d8e5f1cf416968b8
journeyxcode <journeyxcode@gmail.com>
2025-12-28 17:36:17 +0900

    feat:  Added to create a result folder for storing data collection files

--- data-prepare/main.py
+++ data-prepare/main.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 from dotenv import load_dotenv
 
@@ -5,6 +7,14 @@ from preprocessing import TMDBPreProcessor
 from crawler import TMDBCrawler
 
 load_dotenv()
+
+def make_result_folder_crawler():
+    # 현재 실행 중인 파이썬 파일의 절대 경로
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    result_path = os.path.join(script_dir, "result")
+
+    os.makedirs(result_path, exist_ok=True)
+    # print(f"result 폴더 위치: {result_path}")
 
 
 def run_popular_movie_crawler():
@@ -18,4 +28,5 @@ def run_popular_movie_crawler():
 
 
 if __name__ == '__main__':
+    make_result_folder_crawler()
     run_popular_movie_crawler()
